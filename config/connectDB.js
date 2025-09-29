35226efa70f79a9cc06522014c41f282db0b55d1
unknown <tsimkho22@gmail.com>
2025-09-29 20:08:39 +0700

    latest

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,32 +1,34 @@
-const mysql2=require('mysql2');
-const os=require('os');
+const mysql2 = require('mysql2');
+const os = require('os');
+require('dotenv').config();
 
 function getlocalIP(){
-    const interfaces=os.networkInterfaces();
+    const interfaces = os.networkInterfaces();
     for(const name of Object.keys(interfaces)){
         for(const iface of interfaces[name]){
-            if(iface.family==='IPv4'&&!iface.internal && iface.address.startsWith('10.186')){
+            if(iface.family === 'IPv4' && !iface.internal && iface.address.startsWith('10.186')){
                 return iface.address;
             }
         }
     }
-    return 'localhost';
+    return process.env.DB_HOST || 'localhost';
 }
 const localIP = getlocalIP();
 
-const db=mysql2.createConnection({
-    host:localIP,
-    user:'mbdev',
-    password:'96734053',
-    database:'dbkasikam',
+const db = mysql2.createConnection({
+    host: localIP,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     charset: 'utf8mb4',
-})
-db.connect((err)=>{
+});
+
+db.connect((err) => {
     if(err){
         console.log('Error connecting to database:', err);
-    }else{
-        console.log('Connected to database  at',localIP);
+    } else {
+        console.log('Connected to database at', localIP);
     }
-})
+});
 
-module.exports=db;
+module.exports = db;
